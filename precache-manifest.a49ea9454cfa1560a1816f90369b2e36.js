06fd2ff1eb2f568ba25d8187bbf75e267722439f
Vishal Tiwari <itsvishal.codes@gmail.com>
2021-07-27 20:43:18 +0530

    New Portfolio

---
+++ precache-manifest.a49ea9454cfa1560a1816f90369b2e36.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abacc02c08dde8fbd4bb8231095d62ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e14c11ae42433a4d6ef3",
+    "url": "/static/css/2.7b0b64a3.chunk.css"
+  },
+  {
+    "revision": "df8431c1c6608c8dc8d6",
+    "url": "/static/css/main.be50a880.chunk.css"
+  },
+  {
+    "revision": "e14c11ae42433a4d6ef3",
+    "url": "/static/js/2.bd890665.chunk.js"
+  },
+  {
+    "revision": "bc6c744f9986ddec3e9c25f9e2789537",
+    "url": "/static/js/2.bd890665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccb3bea91cb0c26f2e6d",
+    "url": "/static/js/3.41ce2c2d.chunk.js"
+  },
+  {
+    "revision": "df8431c1c6608c8dc8d6",
+    "url": "/static/js/main.378eec6e.chunk.js"
+  },
+  {
+    "revision": "75470a6f6c9fcba246b6",
+    "url": "/static/js/runtime-main.acaf89cd.js"
+  },
+  {
+    "revision": "293c470de2e552a50c3c6028cfbf6aba",
+    "url": "/static/media/Resume.293c470d.png"
+  },
+  {
+    "revision": "3276db1253de058da778886b10920b33",
+    "url": "/static/media/anime-streaming.3276db12.png"
+  },
+  {
+    "revision": "f802a1ffe60de6a23a02cc4ff2b1a57c",
+    "url": "/static/media/covid-awareness.f802a1ff.png"
+  },
+  {
+    "revision": "c30fb6244d1bd377b2a193be25c5ccc4",
+    "url": "/static/media/flipkart-seller.c30fb624.png"
+  },
+  {
+    "revision": "8210ac7276a1af8d5b21f0d002cac35c",
+    "url": "/static/media/healthcare.8210ac72.png"
+  },
+  {
+    "revision": "4c8ccd00db6583023d84035eae89b79b",
+    "url": "/static/media/logo.4c8ccd00.svg"
+  },
+  {
+    "revision": "b41f1706dc464f622f72aa8b2750fe7e",
+    "url": "/static/media/logoDesign.b41f1706.png"
+  },
+  {
+    "revision": "bb03911e7fe1a3a1fb4e44d9cdc3e266",
+    "url": "/static/media/lpu-retouched.bb03911e.png"
+  },
+  {
+    "revision": "617b3998da7fd6cd71859aceab94dd78",
+    "url": "/static/media/portfolio.617b3998.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "145bbd67fddc7704c6cb98ea13c90c65",
+    "url": "/static/media/stock-trainer.145bbd67.png"
+  },
+  {
+    "revision": "107d9562fe90132ea9011259677053fb",
+    "url": "/static/media/thumbPerson.107d9562.png"
+  }
+]);
